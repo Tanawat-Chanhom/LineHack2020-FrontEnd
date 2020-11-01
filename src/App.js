dcb8746f1c3a93c7dbfd9edf2a47106bd64afbf4
Tin Tosanguan <42942811+tinto88@users.noreply.github.com>
2020-11-01 22:58:03 +0700

    Add Register Page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 //Import pages
 import createRoom from "./pages/createRoom/createRoom.js";
 import notFound from "./pages/notFound/notFound";
+import register from "./pages/register/register";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
+          <Route exact path="/register" component={register}></Route>
           <Route exact path="/create-room" component={createRoom}></Route>
           <Route component={notFound}></Route>
         </Switch>
---
+++ src/pages/register/register.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+export default class register extends Component {
+  render() {
+    return (
+      <div>
+        <p>Register</p>
+      </div>
+    );
+  }
+}
