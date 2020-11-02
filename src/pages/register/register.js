b2092923015abf0b872cdd1c8ded73b66903139f
Tin Tosanguan <42942811+tinto88@users.noreply.github.com>
2020-11-02 19:59:09 +0700

    Update Register

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,12 +1,34 @@
+import { TextField } from "@material-ui/core";
 import React, { Component } from "react";
 import "./styles.css";
 
 export default class register extends Component {
-  render() {
-    return (
-      <div>
-        <p>Register</p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className={"container"}>
+                <div>
+                    <h1 className={"title"}>กรุณากรอกข้อมูลวิชาเรียนของท่าน</h1>
+                </div>
+                <div className={"form-container"}>
+                    <div>
+                        <label>ชื่อ</label>
+                        <TextField></TextField>
+                    </div>
+                    <div>
+                        <label>นามสกุล</label>
+                        <TextField></TextField>
+                    </div>
+                    <div>
+                        <label>ชื่อเล่น</label>
+                        <TextField></TextField>
+                    </div>
+                    <div>
+                        <label>เลขที่</label>
+                        <TextField></TextField>
+                    </div>
+                    <button className={"form-button"}>ถัดไป</button>
+                </div>
+            </div>
+        );
+    }
 }
