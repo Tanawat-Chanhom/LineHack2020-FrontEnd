3c959755f5144a53dc9ef96c80a2a6451e977bae
Tin Tosanguan <42942811+tinto88@users.noreply.github.com>
2020-11-03 03:06:28 +0700

    Update Register & Appointment

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,60 +1,138 @@
-import { TextField, Button } from "@material-ui/core";
 import React, { Component } from "react";
-import "./styles.css";
+import style from "./styles.module.css";
+import { Button, TextField } from "@material-ui/core";
+import liff from "@line/liff";
+import cx from "classnames";
 
-export default class register extends Component {
-    render() {
-        return (
-            <div className={"container"}>
-                <div className={"title-2"}>
-                    <label>กรุณากรอกข้อมูลนักเรียนของท่าน</label>
+//Image
+import pic from "../../static/image/check.png";
+
+export default class appointment extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pageState: 0,
+        };
+    }
+
+    secondPage = (
+        <div className={style.elementsContainer2}>
+            <div className={style.titleContainer}>
+                <img src={pic} alt="check-LOGO" />
+                <h1 className={style.titleText}>ลงทะเบียนสำเร็จ</h1>
+            </div>
+        </div>
+    );
+
+    firstPage = (
+        <div className={cx(style.elementsContainer, style.elementsContainer2)}>
+            <div className={style.title}>
+                <label>กรุณากรอกข้อมูลนักเรียนของท่าน</label>
+            </div>
+            <div className={style.inputContainer}>
+                <div className={style.textFieldContainer}>
+                    <label>ชื่อ</label>
+                    <TextField
+                        id="standard-basic"
+                        fullWidth
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        InputProps={{
+                            classes: {
+                                root: {
+
+                                },
+                            },
+                        }}
+                        variant="outlined"
+                    />
                 </div>
-                <div className={"form-container"}>
-                    <div className={"text-field-container"}>
-                        <label>ชื่อ</label>
-                        <TextField
-                            id="outlined-full-width"
-                            label=""
-                            fullWidth
-                            variant="outlined"
-                        />
-                    </div>
-                    <div className={"text-field-container"}>
-                        <label>นามสกุล</label>
-                        <TextField
-                            id="outlined-full-width"
-                            label=""
-                            fullWidth
-                            variant="outlined"
-                        />
-                    </div>
-                    <div className={"text-field-container"}>
-                        <label>ชื่อเล่น</label>
-                        <TextField
-                            id="outlined-full-width"
-                            label=""
-                            fullWidth
-                            variant="outlined"
-                        />
-                    </div>
-                    <div className={"text-field-container"}>
-                        <label>เลขที่</label>
-                        <TextField
-                            id="outlined-full-width"
-                            label=""
-                            fullWidth
-                            variant="outlined"
-                            type="number"
-                        />
-                    </div>
-                    <div className={"button-container"}>
-                        <Button variant="contained" color="primary">
-                            ถัดไป
-                        </Button>
-                    </div>
+                <div className={style.textFieldContainer}>
+                    <label>นามสกุล</label>
+                    <TextField
+                        id="standard-basic"
+                        fullWidth
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        InputProps={{
+                            classes: {
+                                root: {
 
+                                },
+                            },
+                        }}
+                        variant="outlined"
+                    />
+                </div>
+                <div className={style.textFieldContainer}>
+                    <label>ชื่อเล่น</label>
+                    <TextField
+                        id="standard-basic"
+                        fullWidth
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        InputProps={{
+                            classes: {
+                                root: {
+
+                                },
+                            },
+                        }}
+                        variant="outlined"
+                    />
+                </div>
+                <div className={style.textFieldContainer}>
+                    <label>เลขที่</label>
+                    <TextField
+                        id="standard-basic"
+                        type="number"
+                        fullWidth
+                        // InputLabelProps={{
+                        //   shrink: true,
+                        // }}
+                        InputProps={{
+                            disableUnderline: true,
+                        }}
+                        variant="outlined"
+                    />
                 </div>
             </div>
-        );
+            <div className={style.buttonContainer}>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => {
+                        this.setState({
+                            pageState: 1,
+                        });
+                    }}
+                >
+                    ถัดไป
+        </Button>
+            </div>
+        </div>
+    );
+
+    pageState = (state) => {
+        switch (this.state.pageState) {
+            case 0:
+                return this.firstPage;
+
+            case 1:
+                return this.secondPage;
+
+            //   case 2:
+            //     return this.thirdPage(state);
+
+            default:
+                break;
+        }
+    };
+
+    render() {
+        return <div className={style.container}>{this.pageState(this)}</div>;
     }
 }
