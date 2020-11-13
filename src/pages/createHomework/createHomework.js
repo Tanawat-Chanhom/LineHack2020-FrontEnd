267d360c066aaa1a50374dc43858cd561743c5eb
Tanawat_Chanhom <tae_chanhom@hotmail.com>
2020-11-14 00:08:00 +0700

    Update

--- src/pages/createHomework/createHomework.js
+++ src/pages/createHomework/createHomework.js
@@ -5,7 +5,13 @@ import cx from "classnames";
 import MyButton from "../../compoments/button/Button";
 import AlertBar from "../../compoments/AlertBar/AlertBar";
 import DialogBox from "../../compoments/DialogBox/DialogBox";
-import firebase from "../../util/firebaseConfig"
+import firebase from "../../util/firebaseConfig";
+import axios from "axios";
+import ENV from "../../util/env.json";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import liff from "@line/liff";
+// import FinalPage
+
 //Image
 // import TA from "../../static/image/TA-LOGO.png";
 import Edit from "../../static/image/edit@2x.png";
@@ -15,39 +21,28 @@ export default class createHomework extends Component {
   constructor(props) {
     super(props);
     // this.file = null;
-    
+
     this.state = {
       files: [],
-      imagesPreviewUrls: [],
+      completeUrl: [],
       pageState: 0,
+      onProgress: false,
       errorMessage: "",
       alretState: false,
       dialogBox: false,
       dialogType: 0,
-      deleteHomeworkName: "",
+      deleteImageName: "",
       deleteKey: 0,
-      oldHomework: [
-        {
-          homeworkName: "แบบฝึกหัดหลังเรียน บทที่ 1 และ 2",
-          exp: "ส่งพรุ่งนี้",
-          expired: true,
-          sent: true,
-        },
-        {
-          homeworkName: "แบบฝึกหัดหลังเรียน บทที่ 3 และ 4",
-          exp: "เหลืออีก 14 วัน",
-          expired: false,
-          sent: true,
-        },
-        {
-          homeworkName: "บันทึกการอ่าน",
-          exp: "เหลืออีก 28 วัน",
-          expired: false,
-          sent: false,
-        },
-      ],
-      newHomework: [],
+      title: "",
+      sendData: "",
+      sendTime: "",
+      description: "",
+      oldHomework: [],
     };
+  }
+
+  componentDidMount() {
+    this.loadHomework();
   }
 
   _handleImageChange = (e) => {
@@ -60,11 +55,23 @@ export default class createHomework extends Component {
     files.forEach((file, i) => {
       let reader = new FileReader();
 
-      reader.onloadend = () => {
-        this.setState((prevState) => ({
-          files: [...prevState.files, file],
-          imagesPreviewUrls: [...prevState.imagesPreviewUrls, reader.result],
-        }));
+      reader.onloadend = (e) => {
+        // this.setState((prevState) => ({
+        //   files: {
+        //     files: [...prevState.files, file],
+        //     urls: [...prevState.imagesPreviewUrls, reader.result],
+        //   },
+        // }));
+        let body = {
+          file: files[0],
+          base64: e.target.result,
+        };
+        let updateArray = this.state.files;
+        updateArray.push(body);
+        this.setState({
+          files: updateArray,
+        });
+        console.log(this.state.files);
       };
 
       reader.readAsDataURL(file);
@@ -72,105 +79,191 @@ export default class createHomework extends Component {
   };
 
   async _handleSubmit() {
-    console.log("handle uploading-", this.state.files);
-    // e.preventDefault();
-    // TODO: do something with -> this.state.file
+    this.setState({
+      onProgress: true,
+    });
+    let liffContext = liff.getContext();
+    console.log("handle uploading-", this.state.files.file);
     let ref = storage.ref();
-    for(let i = 0;i<this.state.files.length;i++){
-      let image = "homework/"+this.state.files[i].name+"_"+new Date().getTime()+".png"
-      await ref.child(image).put(this.state.files[i])
-      console.log(i)
-      console.log(new Date().getTime())
-      await ref.child(image).getDownloadURL()
-      console.log(await ref.child(image).getDownloadURL())
+    let Urls = [];
+    for (let i = 0; i < this.state.files.length; i++) {
+      let image =
+        "homework/" +
+        this.state.files[i].file.name +
+        "_" +
+        new Date().getTime() +
+        ".png";
+      await ref.child(image).put(this.state.files[i].file);
+      console.log(i);
+      console.log(new Date().getTime());
+      await ref.child(image).getDownloadURL();
+      console.log(await ref.child(image).getDownloadURL());
+      Urls.push(await ref.child(image).getDownloadURL());
     }
+    console.log(Urls);
+
+    let data = this.state.sendData.split("-");
+    let time = this.state.sendTime.split(":");
+    console.log(data, time);
+
+    let body = {
+      groupId: liffContext.groupId,
+      title: this.state.title,
+      expire: new Date(
+        data[0],
+        data[1],
+        data[2],
+        time[0],
+        time[1],
+        0,
+        0
+      ).getTime(),
+      description: this.state.description,
+      files: Urls,
+    };
+    console.log(body);
+    axios
+      .post(ENV.SERVER + "/homework/create", body)
+      .then((response) => {
+        console.log(response);
+        // if (response.data.status === 200) {
+        // this.setState({
+        //   onProgress: false,
+        // });
+        // } else {
+        this.setState({
+          alretState: true,
+          onProgress: false,
+          errorMessage: response.data.message || "Load homework fail!!",
+        });
+        // }
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({
+          alretState: true,
+          onProgress: false,
+          errorMessage: error.message || "Server error",
+        });
+      });
   }
 
-  deleteHomework = (homeworkIndex) => {
-    if (this.state.newHomework.length > 1) {
-      let updateArray = this.state.newHomework;
-      updateArray.splice(homeworkIndex, 1);
-      this.setState({
-        newHomework: updateArray,
+  deleteHomework = (id) => {
+    this.setState({
+      onProgress: true,
+    });
+    axios
+      .delete(ENV.SERVER + "/homework/" + id)
+      .then((response) => {
+        console.log(response);
+        if (response.data.status === 200) {
+          let updateArray = this.state.oldHomework;
+          let index = updateArray.findIndex((X) => X.id === id);
+          updateArray.splice(index, 1);
+          this.setState({
+            oldHomework: updateArray,
+          });
+          this.setState({
+            onProgress: false,
+          });
+        } else {
+          this.setState({
+            alretState: true,
+            onProgress: false,
+            errorMessage: response.data.message || "Load homework fail!!",
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({
+          alretState: true,
+          onProgress: false,
+          errorMessage: error.message || "Server error",
+        });
       });
-      // this.goToQuestion(
-      //   questionIndex + (this.state.currentQuestion === 0 ? 0 : -1)
-      // );
-    }
   };
 
-  firstPage = (state) => (
+  loadHomework = () => {
+    this.setState({
+      onProgress: true,
+    });
+    let liffContext = liff.getContext();
+    axios
+      .get(ENV.SERVER + "/homework/all/" + liffContext.groupId)
+      .then((response) => {
+        console.log(response);
+        if (response.data.status === 200) {
+          this.setState({
+            onProgress: false,
+            oldHomework: response.data.homework,
+          });
+        } else {
+          this.setState({
+            alretState: true,
+            onProgress: false,
+            errorMessage: response.data.message || "Load homework fail!!",
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({
+          alretState: true,
+          onProgress: false,
+          errorMessage: error.message || "Server error",
+        });
+      });
+  };
+
+  firstPage = () => (
     <div className={style.elementsContainer2}>
-      <div className={style.titleContainer}>
-        {/* <img src={TA} alt="TA-LOGO" /> */}
-        <h1 className={style.titleText}>การบ้านทั้งหมดในขณะนี้</h1>
-      </div>
-      <div className={style.homeworkListContainer}>
-        {state.state.oldHomework.length === 0 ? (
-          <label style={{ color: "gray" }}>
-            - ท่านยังไม่ได้สั่งการบ้านใดๆ -
-          </label>
+      <div style={{ width: "100%" }}>
+        <div className={style.titleContainer}>
+          <label className={style.titleText}>การบ้านทั้งหมดในขณะนี้</label>
+        </div>
+        {this.state.onProgress === false ? (
+          <>
+            <div className={style.homeworkListContainer}>
+              {this.state.oldHomework.length === 0 ? (
+                <div style={{ width: "100%", textAlign: "center" }}>
+                  <label style={{ color: "gray", fontSize: 32 }}>
+                    - ท่านยังไม่ได้สั่งการบ้านใดๆ -
+                  </label>
+                </div>
+              ) : (
+                this.state.oldHomework.map((data, key) => {
+                  return (
+                    <div key={key} className={style.homeworkContainer}>
+                      <div className={style.homeworkBox}>
+                        <label style={{ width: "100%", whiteSpace: "nowrap" }}>
+                          {data.title}
+                        </label>
+                        <img src={Edit} alt="Edit" />
+                      </div>
+                      <img
+                        src={DeleteIcon}
+                        alt="DeleteIcon"
+                        onClick={() => {
+                          this.setState({
+                            dialogBox: true,
+                            deleteHomeworkName: data.title,
+                            deleteKey: data.id,
+                          });
+                        }}
+                      />
+                    </div>
+                  );
+                })
+              )}
+            </div>
+          </>
         ) : (
-          state.state.oldHomework.map((data, key) => {
-            if (data.sent === false) {
-              return (
-                <div key={key} className={style.homeworkContainer}>
-                  <div className={style.homeworkBox}>
-                    <label>{data.homeworkName}</label>
-                    <img src={Edit} alt="Edit" />
-                  </div>
-                  <img
-                    src={DeleteIcon}
-                    alt="DeleteIcon"
-                    onClick={() => {
-                      this.setState({
-                        dialogBox: true,
-                        deleteHomeworkName: data.homeworkName,
-                      });
-                    }}
-                  />
-                </div>
-              );
-            } else if (data.sent === true) {
-              return (
-                <div key={key} className={style.homeworkContainer}>
-                  <div className={style.homeworkBox}>
-                    <label>{data.homeworkName}</label>
-                    <img src={Edit} alt="Edit" />
-                  </div>
-                  <img
-                    src={DeleteIcon}
-                    alt="DeleteIcon"
-                    onClick={() => {
-                      this.setState({
-                        dialogBox: true,
-                        deleteHomeworkName: data.homeworkName,
-                      });
-                    }}
-                  />
-                </div>
-              );
-            } else if (data.expired === true) {
-              return (
-                <div key={key} className={style.homeworkContainer}>
-                  <div className={style.homeworkBox}>
-                    <label>{data.homeworkName}</label>
-                    <img src={Edit} alt="Edit" />
-                  </div>
-                  <img
-                    src={DeleteIcon}
-                    alt="DeleteIcon"
-                    onClick={() => {
-                      this.setState({
-                        dialogBox: true,
-                        deleteHomeworkName: data.homeworkName,
-                      });
-                    }}
-                  />
-                </div>
-              );
-            }
-          })
+          <center>
+            <CircularProgress
+              style={{ display: "inline-block", color: "#e5a52d", margin: 10 }}
+            ></CircularProgress>
+          </center>
         )}
       </div>
       <div className={style.buttonContainer}>
@@ -188,44 +281,89 @@ export default class createHomework extends Component {
     </div>
   );
 
-  secondPage = (state) => (
+  secondPage = () => (
     <div className={cx(style.elementsContainer, style.elementsContainer2)}>
       <div className={style.inputContainer}>
         <div className={style.textFieldContainer}>
           <label>ระบุชื่อการบ้าน</label>
-          <TextField fullWidth variant="outlined" className={style.textFieldTitle}/>
+          <TextField
+            fullWidth
+            variant="outlined"
+            className={style.textFieldTitle}
+            value={this.state.title}
+            onChange={(event) => {
+              this.setState({
+                title: event.target.value,
+              });
+            }}
+          />
         </div>
         <div
           className={cx(style.textFieldContainer, style.textFieldContainer2)}
         >
           <label>วันที่ส่งการบ้าน</label>
-          <TextField type="date" fullWidth variant="outlined" />
+          <TextField
+            type="date"
+            fullWidth
+            variant="outlined"
+            value={this.state.sendData}
+            onChange={(event) => {
+              this.setState({
+                sendData: event.target.value,
+              });
+            }}
+          />
         </div>
         <div
           className={cx(style.textFieldContainer, style.textFieldContainer2)}
         >
           <label>ก่อนเวลา </label>
           <div className={style.timeContainer}>
-            <TextField type="time" variant="outlined" />
+            <TextField
+              type="time"
+              variant="outlined"
+              value={this.state.sendTime}
+              onChange={(event) => {
+                this.setState({
+                  sendTime: event.target.value,
+                });
+              }}
+            />
           </div>
         </div>
         <div className={cx(style.textFieldContainer, style.textAreaContainer)}>
           <label>คำอธิบายเพิ่มเติม</label>
           <div className={style.areaContainer}>
-            <TextField fullWidth multiline rows={6} variant="outlined" />
-            {state.state.imagesPreviewUrls.map((data, key) => {
+            <TextField
+              fullWidth
+              multiline
+              rows={6}
+              variant="outlined"
+              value={this.state.description}
+              onChange={(event) => {
+                this.setState({
+                  description: event.target.value,
+                });
+              }}
+            />
+            {this.state.files.map((data, key) => {
               return (
                 <div className={style.imguploadContainer}>
-                  <img key={key} src={data} className={style.imgupload} />
+                  <img
+                    key={key}
+                    src={data.base64}
+                    className={style.imgupload}
+                  />
                   <img
                     src={DeleteIcon}
                     alt="DeleteIcon"
                     className={style.imguploaddel}
                     onClick={() => {
-                      // this.setState({
-                      //   dialogBox: true,
-                      //   deleteHomeworkName: data.homeworkName,
-                      // });
+                      this.setState({
+                        dialogBox: true,
+                        deleteHomeworkName: data.homeworkName,
+                      });
+                      this.deleteImage(data.file.name);
                     }}
                   />
                   {/* <label1>{state.state.files[key].name}</label1> */}
@@ -274,7 +412,7 @@ export default class createHomework extends Component {
     </div>
   );
 
-  thirdPage = (
+  thirdPage = () => (
     <div className={cx(style.elementsContainer, style.elementsContainer2)}>
       <div className={style.inputContainer}>
         <div className={cx(style.textFieldContainer, style.textAreaContainer)}>
@@ -305,17 +443,14 @@ export default class createHomework extends Component {
     </div>
   );
 
-  pageState = (state) => {
+  pageState = () => {
     switch (this.state.pageState) {
       case 0:
-        return this.firstPage(state);
-
+        return this.firstPage();
       case 1:
-        return this.secondPage(state);
-
+        return this.secondPage();
       case 2:
-        return this.thirdPage;
-
+        return this.thirdPage();
       default:
         break;
     }
@@ -334,7 +469,7 @@ export default class createHomework extends Component {
   render() {
     return (
       <div className={style.container}>
-        {this.pageState(this)}
+        {this.pageState()}
 
         <AlertBar
           open={this.state.alretState}
@@ -358,10 +493,7 @@ export default class createHomework extends Component {
             });
           }}
           onSubmit={() => {
-            if (this.state.dialogType === 0) {
-            } else {
-              this.deleteHomework(this.state.deleteKey);
-            }
+            this.deleteHomework(this.state.deleteKey);
           }}
         ></DialogBox>
       </div>
